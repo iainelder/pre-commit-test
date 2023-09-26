33d66c700ec1e4f857105b58d89a4ab95207a9d6
Iain Samuel McLean Elder <iain@isme.es>
2023-09-26 12:31:29 +0200

    Configure remark with consistent preset

---
+++ .remarkrc.js
@@ -0,0 +1,9 @@
+import remarkPresetLintConsistent from 'remark-preset-lint-consistent'
+
+const remarkConfig = {
+    plugins: [
+        remarkPresetLintConsistent
+    ]
+}
+
+export default remarkConfig
